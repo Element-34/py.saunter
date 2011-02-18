26a33aa775e67a117f11beea044e03c83954ca0c
Adam Goucher <adam@goucher.ca>
2011-02-18 12:27:29 -0500

    downloading of the ondemand video

--- modules/CustomTestCase.py
+++ modules/CustomTestCase.py
@@ -6,13 +6,14 @@ from SeleniumWrapper import SeleniumWrapper as wrapper
 
 import ConfigWrapper
 
+if ConfigWrapper.ConfigWrapper().config.getboolean("SauceLabs", "ondemand"):
+    import json
+
 class CustomTestCase(unittest.TestCase):
     def setUp(self):
         self.verificationErrors = []
         self.cf = ConfigWrapper.ConfigWrapper().config
         if self.cf.getboolean("SauceLabs", "ondemand"):
-            import json
-            
             host = self.cf.get("SauceLabs", "server_host")
             port = self.cf.get("SauceLabs", "server_port")
             j = {}
@@ -29,6 +30,10 @@ class CustomTestCase(unittest.TestCase):
 
         self.selenium = wrapper().connect(host, port, browser, self.cf.get("Selenium", "base_url"))
         self.selenium.start()
+        
+        if self.cf.getboolean("SauceLabs", "ondemand"):
+            self.sauce_session = self.selenium.get_eval("selenium.sessionId")
+        
         self.selenium.window_maximize()
         self.selenium.open('/');
 
@@ -53,7 +58,40 @@ class CustomTestCase(unittest.TestCase):
             # tags
             j["tags"] = getattr(getattr(self, self._testMethodName), 'tags')
             self.selenium.set_context('sauce: job-info=%s' % json.dumps(j))
+
         self.selenium.stop()
+
+        if self.cf.getboolean("SauceLabs", "ondemand"):
+            if self.cf.getboolean("SauceLabs", "get_video"):
+                import os.path
+                import time
+                import urllib2
+                
+                auth_handler = urllib2.HTTPBasicAuthHandler()
+                auth_handler.add_password("Sauce", "https://saucelabs.com/", self.cf.get("SauceLabs", "username"), self.cf.get("SauceLabs", "key"))
+                opener = urllib2.build_opener(auth_handler)
+                urllib2.install_opener(opener)
+
+                video_url = "https://saucelabs.com/rest/%s/jobs/%s/results/video.flv" % (self.cf.get("SauceLabs", "username"), self.sauce_session)
+                code = 404
+                while code == 404:
+                    req = urllib2.Request(video_url)
+                    try:
+                        response = urllib2.urlopen(req)
+                        # implicit
+                        code = 200
+                    except urllib2.URLError, e:
+                        if e.code == 404:
+                            code = e.code
+                            time.sleep(2)
+                        if e.code == 401:
+                            print("401'ing -- this shouldn't be happening...")
+                            break
+                            
+                video = open(os.path.join(os.path.dirname(__file__), "..", "logs", "video.flv"), "wb")
+                video.write(response.read())
+
+        
         self.assertEqual([], self.verificationErrors)
 
     def verifyEqual(self, want, got):
