6bb7f9257178046a774dd9550fc216ccfec48e17
Adam Goucher <adam@goucher.ca>
2011-02-17 23:33:01 -0500

    basic ondemand integration

--- modules/CustomTestCase.py
+++ modules/CustomTestCase.py
@@ -1,5 +1,6 @@
 import unittest2 as unittest
 
+import json
 import logging
 
 from SeleniumWrapper import SeleniumWrapper as wrapper
@@ -12,10 +13,22 @@ class CustomTestCase(unittest.TestCase):
     def setUp(self):
         self.verificationErrors = []
         self.cf = ConfigWrapper.ConfigWrapper().config
-        self.selenium = wrapper().connect(self.cf.get("Selenium", "server_host"), \
-                                          self.cf.get("Selenium", "server_port"), \
-                                          self.cf.get("Selenium", "browser"), \
-                                          self.cf.get("Selenium", "base_url"))
+        if self.cf.get("SauceLabs", "ondemand"):
+            host = self.cf.get("SauceLabs", "server_host")
+            port = self.cf.get("SauceLabs", "server_port")
+            j = {}
+            j['username'] = self.cf.get("SauceLabs", "username")
+            j['access-key'] = self.cf.get("SauceLabs", "key")
+            j['os'] = self.cf.get("SauceLabs", "os")
+            j['browser'] = self.cf.get("SauceLabs", "browser")
+            j['browser-version'] = self.cf.get("SauceLabs", "browser_version")
+            browser = json.dumps(j)
+        else:
+            host = self.cf.get("Selenium", "server_host")
+            port = self.cf.get("Selenium", "server_port")
+            browser = self.cf.get("Selenium", "browser")
+
+        self.selenium = wrapper().connect(host, port, browser, self.cf.get("Selenium", "base_url"))
         self.selenium.start()
         self.selenium.window_maximize()
         self.selenium.open('/');
