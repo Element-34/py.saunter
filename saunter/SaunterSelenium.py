18b0d99bc89061573ac689eddaeb99d07ad474ad
Adam Goucher <adam@goucher.ca>
2011-08-07 13:48:07 -0400

    implicit waits can now be controlled by saunter.ini

--- saunter/SaunterSelenium.py
+++ saunter/SaunterSelenium.py
@@ -14,6 +14,7 @@
 import re
 import time
 from selenium import selenium
+import saunter.ConfigWrapper
 
 class SaunterSelenium(selenium):
     """
@@ -21,18 +22,26 @@ class SaunterSelenium(selenium):
     """
 
     def __init__(self, *args):
-        self.implicit_wait = 30
         super(SaunterSelenium, self).__init__(*args)
+        
+        try:
+            if saunter.ConfigWrapper.ConfigWrapper().config.getboolean("Saunter", "use_implicit_wait"):
+                self.implicit_wait = saunter.ConfigWrapper.ConfigWrapper().config.getint("Saunter", "implicit_wait")
+            else:
+                self.implicit_wait = 0
+        except:
+            self.implicit_wait = 0
 
-    def do_command(self, verb, args, implicit_wait=None):
-        if implicit_wait is None:
+    def do_command(self, verb, args, implicit_wait = None):
+        if implicit_wait == None:
             implicit_wait = self.implicit_wait
-
+            
         try:
             return super(SaunterSelenium, self).do_command(verb, args)
         except Exception, e:
             if (re.match("ERROR: Element .* not found", str(e))
                 and implicit_wait > 0):
+                print('implicit')
                 time.sleep(1)
-                return self.do_command(verb, args, implicit_wait-1)
+                return self.do_command(verb, args, implicit_wait - 1)
             raise Exception(e)
