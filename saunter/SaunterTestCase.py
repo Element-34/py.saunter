d0cdac36e7df3d4a9da8fe1f24c2a1975d6989e2
Adam Goucher <adam@goucher.ca>
2011-08-14 21:31:27 -0400

    adding some waits

--- saunter/SaunterTestCase.py
+++ saunter/SaunterTestCase.py
@@ -127,11 +127,32 @@ class SaunterTestCase(unittest.TestCase):
         
         self.assertEqual([], self.verificationErrors)
 
-    def verifyEqual(self, want, got):
-        """
-        'Soft' assert for checking whether two things are equal
-        """
+    def verify_equal(self, want, got):
         try:
             self.assertEqual(want, got)
         except AssertionError, e:
             self.verificationErrors.append(str(e))
+
+    def verify_text_present(self, text):
+        try:
+            self.assertTrue(self.selenium.is_text_present(text))
+        except AssertionError, e:
+            self.verificationErrors.append(str(e))
+
+    def verify_element_present(self, locator):
+        try:
+            self.assertTrue(self.selenium.is_element_present(locator))
+        except AssertionError, e:
+            self.verificationErrors.append(str(e))
+
+    def verify_visible(self, locator):
+        try:
+            self.assertTrue(self.selenium.is_visible(locator))
+        except AssertionError, e:
+            self.verificationErrors.append(str(e))
+
+    def verify_true(self, condition):
+        try:
+            self.assertTrue(condition)
+        except AssertionError, e:
+            self.verificationErrors.append(str(e))            
