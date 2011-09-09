65595b46f4fd6cd66d5d4d05303c0deb135487f7
Adam Goucher <adam@goucher.ca>
2011-09-08 22:36:06 -0400

    adding the apache headers. eventually i will remember to do this the first time

--- examples/saucelabs/modules/tailored/remotecontrol.py
+++ examples/saucelabs/modules/tailored/remotecontrol.py
@@ -1,6 +1,27 @@
+# Copyright 2011 Element 34
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+=============
+RemoteControl
+=============
+"""
 from saunter.SaunterSelenium import SaunterSelenium
 
 class RemoteControl(SaunterSelenium):
+    """
+    Modifications to the core Remote Control API are done in this class
+    """
     def __init__(self, *args):
         super(RemoteControl, self).__init__(*args)
         
