9f0fe642b4e1a93044815a34da13d1c1b1beca82
Adam Goucher <adam@goucher.ca>
2011-09-14 23:39:06 -0400

    whoops. apparently you cant do x.y.z in __version__

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
                               "_defaults/pytest.ini",
                               "_defaults/conf/saunter.ini.default",
                               "_defaults/tailored/remotecontrol.py"]},
-    version = "0.10.1",
+    version = "0.11",
     author = "adam goucher",
     author_email = "adam@element34.ca",
     install_requires = ['pytest>2.0.2',
