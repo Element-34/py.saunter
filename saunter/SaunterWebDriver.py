fa39b882aa5b5327bf05acb9ba3f8014464c8a02
Adam Goucher <adam@goucher.ca>
2011-10-17 21:55:51 -0400

    adding find_elements_by_locator -- in a yucky manner that is sure to make greg cringe...

--- saunter/SaunterWebDriver.py
+++ saunter/SaunterWebDriver.py
@@ -44,7 +44,34 @@ class SaunterWebDriver(object):
             return driver.find_element_by_xpath(locator_value)
         else:
             raise saunter.exceptions.InvalidLocatorString(locator)
-            
+
+    @classmethod
+    def find_elements_by_locator(cls, locator):
+        driver = se_wrapper().connection
+
+        locator_type = locator[:locator.find("=")]
+        if locator_type == "":
+            raise saunter.exceptions.InvalidLocatorString(locator)
+        locator_value = locator[locator.find("=") + 1:]
+        if locator_type == 'class':
+            return driver.find_elements_by_class_name(locator_value)
+        elif locator_type == 'css':
+            return driver.find_elements_by_css_selector(locator_value)
+        elif locator_type == 'id':
+            return driver.find_elements_by_id(locator_value)
+        elif locator_type == 'link':
+            return driver.find_elements_by_link_text(locator_value)
+        elif locator_type == 'name':
+            return driver.find_elements_by_name(locator_value)
+        elif locator_type == 'plink':
+            return driver.find_elements_by_partial_link_text(locator_value)
+        elif locator_type == 'tag':
+            return driver.find_elements_by_tag_name(locator_value)
+        elif locator_type == 'xpath':
+            return driver.find_elements_by_xpath(locator_value)
+        else:
+            raise saunter.exceptions.InvalidLocatorString(locator)
+
     @classmethod
     def click(cls, locator):
         driver = se_wrapper().connection
