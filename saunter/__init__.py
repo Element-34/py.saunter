522a18ae3327304f309ad057617cb994815e3329
Adam Goucher <adam@goucher.ca>
2011-11-22 16:18:00 -0500

    version bounce while i remember it

--- saunter/__init__.py
+++ saunter/__init__.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = 0.25
+__version__ = 0.26
 __all__ = ['generators', 'po', 'providers', 'testcase']
