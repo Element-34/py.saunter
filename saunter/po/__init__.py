590f2856fb5874463570f9128bdb42214195cc15
Adam Goucher <adam@goucher.ca>
2011-12-15 11:30:10 -0500

    timeouts are now configurable. at least for rc

--- saunter/po/__init__.py
+++ saunter/po/__init__.py
@@ -17,8 +17,15 @@
 Common
 ======
 """
+
+import saunter.ConfigWrapper
+
+cf = saunter.ConfigWrapper.ConfigWrapper().config
 #: timeout value in s as an integer
-timeout_seconds = 30
+if cf.has_option("Selenium", "timeout"):
+    timeout_seconds = cf.getint("Selenium", "timeout")
+else:
+    timeout_seconds = 30
 #: timout value in ms as an integer
 timeout_microseconds = timeout_seconds * 1000
 #: timout value in ms as a string
