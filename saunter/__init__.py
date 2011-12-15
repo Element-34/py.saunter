48a92f3b84800992aa6e55a3a153e2d98369f915
Adam Goucher <adam@goucher.ca>
2011-12-15 11:31:19 -0500

    version bump

--- saunter/__init__.py
+++ saunter/__init__.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = 0.29
+__version__ = 0.30
 __all__ = ['generators', 'po', 'providers', 'testcase']
