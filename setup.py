1592a390dd326407baf947221749f9ea67582f16
Adam Goucher <adam@goucher.ca>
2011-12-29 21:13:05 -0500

    version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
                               "_defaults/pytest.ini",
                               "_defaults/conf/saunter.ini.default",
                               "_defaults/tailored/remotecontrol.py"]},
-    version = "0.31",
+    version = "0.32",
     author = "adam goucher",
     author_email = "adam@element34.ca",
     install_requires = ['pytest==2.2.0',
