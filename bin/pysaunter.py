26021258cf8772d9e35d2355b2ddc9f43b9c0948
Adam Goucher <adam@goucher.ca>
2011-12-30 14:26:32 -0500

    properly disabling --durations

--- bin/pysaunter.py
+++ bin/pysaunter.py
@@ -109,7 +109,7 @@ p.add_argument('--traceconfig', action='store_true', default=None, help="trace c
 p.add_argument('--pdb', action='store_true', default=None, help="start the interactive Python debugger on errors")
 p.add_argument('--maxfail', action='store', default=None, help="exit after first num failures or errors.")
 p.add_argument('--collectonly', action='store_true', default=None, help="only collect tests, don't execute them")
-p.add_argument('--durations', action='store', default="None", help='show N slowest setup/test durations (N=0 for all)')
+p.add_argument('--durations', action='store', default=None, help='show N slowest setup/test durations (N=0 for all)')
 
 results = p.parse_args()
 
