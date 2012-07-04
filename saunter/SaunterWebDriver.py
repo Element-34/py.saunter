1210d764e49ef536aa4cb84c599b8970efdcf2a1
Adam Goucher <adam@goucher.ca>
2012-07-04 16:03:12 -0400

    wrapping elements in own class so that find_element_by_locator works not just from the driver but from elements as well

--- saunter/SaunterWebDriver.py
+++ saunter/SaunterWebDriver.py
@@ -16,6 +16,7 @@ import saunter.exceptions
 from selenium import webdriver
 from saunter.SeleniumWrapper import SeleniumWrapper as se_wrapper
 from selenium.common.exceptions import NoSuchElementException
+from saunter.web_element import WebElement
 
 class SaunterWebDriver(webdriver.Remote):
     def find_element_by_locator(self, locator):
@@ -24,21 +25,21 @@ class SaunterWebDriver(webdriver.Remote):
             raise saunter.exceptions.InvalidLocatorString(locator)
         locator_value = locator[locator.find("=") + 1:]
         if locator_type == 'class':
-            return self.find_element_by_class_name(locator_value)
+            return WebElement(self.find_element_by_class_name(locator_value))
         elif locator_type == 'css':
-            return self.find_element_by_css_selector(locator_value)
+            return WebElement(self.find_element_by_css_selector(locator_value))
         elif locator_type == 'id':
-            return self.find_element_by_id(locator_value)
+            return WebElement(self.find_element_by_id(locator_value))
         elif locator_type == 'link':
-            return self.find_element_by_link_text(locator_value)
+            return WebElement(self.find_element_by_link_text(locator_value))
         elif locator_type == 'name':
-            return self.find_element_by_name(locator_value)
+            return WebElement(self.find_element_by_name(locator_value))
         elif locator_type == 'plink':
-            return self.find_element_by_partial_link_text(locator_value)
+            return WebElement(self.find_element_by_partial_link_text(locator_value))
         elif locator_type == 'tag':
-            return self.find_element_by_tag_name(locator_value)
+            return WebElement(self.find_element_by_tag_name(locator_value))
         elif locator_type == 'xpath':
-            return self.find_element_by_xpath(locator_value)
+            return WebElement(self.find_element_by_xpath(locator_value))
         else:
             raise saunter.exceptions.InvalidLocatorString(locator)
 
