de1c6341f006458daa92c80b8180e241c083c162
Adam Goucher <adam@goucher.ca>
2012-07-04 15:51:50 -0400

    fixing an errant multi-line comment

--- saunter/_defaults/tailored/page.py
+++ saunter/_defaults/tailored/page.py
@@ -1,4 +1,3 @@
-from saunter.po.webdriver.page import Page as SaunterPage
 # Copyright 2011 Element 34
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +11,8 @@ from saunter.po.webdriver.page import Page as SaunterPage
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
+
+from saunter.po.webdriver.page import Page as SaunterPage
 
 from saunter.po import timeout_seconds
 from selenium.webdriver.support.ui import WebDriverWait
